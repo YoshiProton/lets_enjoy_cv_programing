d43b8d8807a3386ede519b10509c3887516ae308
sakiyamaK <sakiyama.k@gmail.com>
2016-06-26 20:47:14 +0900

    first commit

---
+++ sakiyama/cv_beginers/cv_beginers/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  cv_beginers
+//
+//  Created by 崎山 圭 on 2016/06/26.
+//  Copyright © 2016年 崎山 圭. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+  cv::Mat test;
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
