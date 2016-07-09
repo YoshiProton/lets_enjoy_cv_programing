229c412c9f491f4107d754ae870cfca6ec7decaf
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2016-07-09 17:08:37 +0900

    load lena and fix gitignore

--- toyoshi/opencv_sample.cpp
+++ toyoshi/opencv_sample.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main()
+using namespace std;
+using namespace cv;
+
+int main( int argc, char** argv )
 {
-  std::cout << "hell world" << std::endl;
+  if( argc != 2)
+    {
+      cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+      return -1;
+    }
+
+  Mat image;
+  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+  if(! image.data )
+    {
+      cout <<  "Could not open or find the image" << std::endl ;
+      return -1;
+    }
+
+  namedWindow( "Display window", WINDOW_AUTOSIZE );
+  imshow( "Display window", image );
+
+  waitKey(0);
+  return 0;
 }
