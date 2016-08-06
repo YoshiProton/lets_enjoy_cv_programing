c0cb0f1c54b5a3b096e265f9be7ebac01684cd76
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2016-08-06 13:46:39 +0900

    add lesson2

--- toyoshi/toyocv/modules/imgproc/src/NearestNeighborsResizer.cpp
+++ toyoshi/toyocv/modules/imgproc/src/NearestNeighborsResizer.cpp
@@ -6,8 +6,8 @@ namespace toyocv{
     int imageWidth = src.rows;
     int imageHeight = src.cols;
   
-    for(int w = 0; w < imageWidth; w++){
-      for(int h = 0; h < imageHeight; h++){
+    for(int w = 0; w < imageWidth; ++w){
+      for(int h = 0; h < imageHeight; ++h){
 	Vec3b p = src.at<Vec3b>(w, h);
 
 	if (w != 0 && h != 0){
@@ -31,11 +31,11 @@ namespace toyocv{
     int imageWidth = src.rows;
     int imageHeight = src.cols;
   
-    for(int w = 0; w < imageWidth; w++){
+    for(int w = 0; w < imageWidth; ++w){
       cv::Vec3b *srcPointer1 = src.ptr<cv::Vec3b>(w * 2 - 1);
       cv::Vec3b *srcPointer2 = src.ptr<cv::Vec3b>(w * 2);
 
-      for(int h = 0; h < imageHeight; h++){
+      for(int h = 0; h < imageHeight; ++h){
 	Vec3b p = src.at<Vec3b>(w, h);
 
 	if (w != 0 && h != 0){
